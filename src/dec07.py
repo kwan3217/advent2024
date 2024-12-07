2122e1be6db487d5c651d95c36a34e8077c0d71d
kwan3217 <kwan3217@gmail.com>
2024-12-07 10:35:13 -0700

    Solved part A. It helps when I figure out how to use the cartesian product function :)

--- src/dec07.py
+++ src/dec07.py
@@ -3,9 +3,18 @@ Brute force and Ignorance! O(2**n) here we go!
 
 Created: 12/7/24
 """
+from functools import partial
+from itertools import combinations_with_replacement, permutations, product
+from typing import Iterable, Callable
+from multiprocessing import Pool
+
+StrEqns=list[str]
+Terms=list[int]
+Eqn=tuple[int,Terms]
+Eqns=list[Eqn]
 
 
-def format_eqns(str_eqns:list[str])->list[tuple[int,list[int]]]:
+def format_eqns(str_eqns:StrEqns)->Eqns:
     result=[]
     for str_eqn in [str_eqn for str_eqn in str_eqns if str_eqn.strip()!=""]:
         l,r=[x.strip() for x in str_eqn.split(":")]
@@ -27,12 +36,55 @@ test_eqns=format_eqns([
 ])
 
 
+def permutations_with_replacement(items:Iterable,n:int):
+    for combo in combinations_with_replacement(items,n):
+        for perm in permutations(combo):
+            yield perm
+
+
+Operator=Callable[[int,int],int]
+Operators=Iterable[Operator]
+
+def check_one_eqn(*args,operators:Operators)->bool:
+    i_eqn, eqn=args[0]
+    answer,terms=eqn
+    print(f"{i_eqn} - {answer}: {terms}")
+    print(f"Number of possibilities: {len(operators)**(len(terms)-1)}")
+    for i_perm,perm_operators in enumerate(product(operators,repeat=len(terms[1:]))):
+        this_result = terms[0]
+        for term,operator in zip(terms[1:],perm_operators):
+            this_result=operator(this_result,term)
+        if this_result==answer:
+            return True
+
+
+operators_a = [lambda a, b: a + b, lambda a, b: a * b]
+
+
+def dec07(eqns:Eqns,operators:Operators)->int:
+    result=0
+    numbered_eqns = list(enumerate(eqns))
+    map_results=[check_one_eqn(numbered_eqn,operators=operators) for numbered_eqn in numbered_eqns]
+    for map_result,(answer,terms) in zip(map_results,eqns):
+        if map_result:
+            result+=answer
+    return result
+
+
 def test_dec07a():
-    assert dec07a(test_eqns)==3749
+    assert dec07(test_eqns,operators_a)==3749
+
+
+def read_dec07()->Eqns:
+    with open("data/dec07.txt","rt") as inf:
+        result=[line for line in inf if line!=""]
+    return format_eqns(result)
 
 
 def main():
     test_dec07a()
+    eqns=read_dec07()
+    print(dec07(eqns,operators_a))
     test_dec07b()
 
 
