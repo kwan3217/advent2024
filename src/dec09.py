3061f5ff59334016a4852b9c013c96334fc9c746
kwan3217 <kwan3217@gmail.com>
2024-12-09 14:54:04 -0700

    Step by step commit

---
+++ src/dec09.py
@@ -0,0 +1,24 @@
+"""
+Trying a different way of documenting things -- typing out my thoughts in the code.
+
+First, we create the test cases which will fail
+
+Created: 12/9/24
+"""
+
+
+def test_dec09a():
+    assert dec09a(test_data)==None
+
+
+def test_dec09b():
+    assert dec09b(test_data)==None
+
+
+def main():
+    test_dec09a()
+    test_dec09b()
+
+
+if __name__ == "__main__":
+    main()
