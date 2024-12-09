ecda8cbd714cfae93eb0f11967e080e9c9183f41
kwan3217 <kwan3217@gmail.com>
2024-12-09 14:55:41 -0700

    Step by step commit

--- src/dec09.py
+++ src/dec09.py
@@ -3,8 +3,18 @@ Trying a different way of documenting things -- typing out my thoughts in the co
 
 First, we create the test cases which will fail
 
+Now read the problem. Amphipods! Oh no! The test data is there, so grab it.
+
+
+
 Created: 12/9/24
 """
+
+test_disk_map="2333133121414131402"
+
+
+def dec09a(disk_map:str)->int:
+    pass
 
 
 def test_dec09a():
